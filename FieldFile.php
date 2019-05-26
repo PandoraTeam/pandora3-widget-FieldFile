4e98adfeabe658c9d0a959a64c9494653db751f4
Petr Gostin <U.238@rambler.ru>
2019-05-26 17:28:49 +0300

    initial commit

--- FieldFile.php
+++ FieldFile.php
@@ -1,1 +1,21 @@
+<?php
+namespace Pandora3\Widgets\FieldFile;
 
+use Pandora3\Widgets\FormField\FormField;
+
+class FieldFile extends FormField {
+
+	protected function getView(): string {
+		return __DIR__.'/Views/Widget';
+	}
+
+	protected function beforeRender(array $context): array {
+		$context = parent::beforeRender($context);
+		if ($context['disabled'] ?? false) {
+			$attribs = $context['attribs'] ?? '';
+			$context['attribs'] = $attribs.' disabled';
+		}
+		return $context;
+	}
+
+}
---
+++ Views/Widget.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @var string $name
+ */
+$class = $class ?? '';
+$attribs = $attribs ?? '';
+
+?><input type="file" class="<?= $class ?>" name="<?= $name ?>"><?php
